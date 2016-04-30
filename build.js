37f914f95c22db38981c1ee07d9fa4b3ec1f85d3
chaffin <larry.chaffin@yahoo.com>
2016-04-30 11:35:38 -0500

    Initial commit.

---
+++ build.js
@@ -0,0 +1,91 @@
+/*                            C O N G R U E N T - 2 0 1 6
+ *                            ===========================
+ * Congruent is a collaborative review  process  that utilizes [Jargon: a technical
+ * framework] that enables authors to write more concise  sentences and phrases for
+ * their works. Congruent, as a process allow authors to ensure standards are clear
+ * to improve quality and performance while promoting original intent.
+ *
+*/
+var y = 0;
+var n = 0;
+var r = [];
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l"];
+document.getElementById("head").innerHTML = "<title>Congruent Formatter<head></title><style>div#page{margin:auto;max-width:48em;color:#303030;font-family:sans-serif;}h1{text-align:center;color:#606060;}textarea{overflow:auto;width:48em;height:500px;border:3px solid #cccccc;padding:5px;font-family:sans-serif;background-color:#ffe;background-position:bottom right;}input{background-color:#ffa;}</style></head>";
+document.write("<div id=\"page\"><p><h1>Format Congruent File</h1><b>File:</b> " + (json.Metadata.topic) + "<br><b>Date:</b> " + (json.Metadata.Draft.date) + "</p><p><b>Description</b><br> " + (json.Metadata.description) + "</p>");
+document.write("<textarea rows=\"24\" cols=\"120\" wrap=\"soft\">");
+//  metadata
+    document.write("<!DOCTYPE html>\n<html>\n<head>\n<title>" + (json.Metadata.topic) + "</title>\n<meta name=\"UID\" content=\"" + (json.Metadata.UID) + "\">\n<meta name=\"topic\" content=\"" + (json.Metadata.topic) + "\">\n<meta name=\"category\" content=\"" + (json.Metadata.category) + "\">\n<meta name=\"description\" content=\"" + (json.Metadata.description) + "\">\n<meta name=\"keywords\" content=\"" + (json.Metadata.keywords) + "\">\n<meta name=\"author\" content=\"" + (json.Metadata.Draft.Author.name) + "\">\n<meta name=\"reviewer\" content=\"" + (json.Metadata.Review.Author.name) + "\">\n<meta name=\"review-date\" content=\"" + (json.Metadata.Review.date) + "\">\n<meta name=\"congruent\" content=\"" + (json.Metadata.Congruent.Author.name) + "\">\n<meta name=\"congruent-date\" content=\"" + (json.Metadata.Congruent.date) + "\">\n<meta name=\"status\" content=\"" + (json.Metadata.status) + "\">\n<link rel=\"stylesheet\" href=\"congruent.css\">\n<script src=\"http://code.jquery.com/jquery-1.12.2.min.js\"></script>\n<script src=\"congruent.js\"></script>\n</head>\n<body>\n<nav id=\"slide-menu\">\n<ul>\n<li class=\"abstract\"><a href=\"#abstract\">Abstract</a></li>\n<li class=\"section\"><a href=\"#introduction\">Introduction</a></li>\n<li class=\"disclaimer\"><a href=\"#disclaimer\">Disclaimer</a></li>\n<li class=\"copyright\"><a href=\"#copyright\">Copyright</a></li>\n<li class=\"references\"><a href=\"#references\">References</a></li>\n<li class=\"congruence\"><a href=\"#notes\">Notes</a></li>\n<li class=\"sep congruent\"><a href=\"#congruence\">Congruence</a></li>\n<li class=\"jargon-ico\"><a href=\"#jargon\">Jargon</a></li>\n<li class=\"sep\"></li>\n</ul>\n<div id=\"quote\"></div>\n<footer>\n<p>&amp;cong; CONGRUENT &amp;copy;2016 &amp;middot; Larry Chaffin</p>\n</footer>\n</nav>\n<a id=\"top\"></a>\n<div id=\"content\">\n<div class=\"menu-trigger\"  onclick=\"myFunction()\"></div>\n<div class=\"closeall\"></div>\n<div class=\"expand\"></div>\n<div class=\"page\">\n<p id=\"col8\">&amp;cong; " + (json.Metadata.status) + "<br>\n" + (json.Metadata.Draft.date) + "</p>\n<p class=\"title\">" + (json.Metadata.topic) + "</p>\n<a id=\"abstract\"></a>\n<p class=\"head\">Abstract</p>\n");
+// abstract
+var s = json.Metadata.status;
+for (i in Congruent.Abstract[s]) {
+  document.write("<p class=\"para\">" + (Congruent.Abstract[s][i]) + "</p>\n");
+}
+// keywords
+  document.write("<p id=\"col3\" class=\"para\"><em>Keywords:</em> " + (json.Metadata.keywords) + "</p>\n");
+// reference list
+  document.write("<p class=\"head\">References</p>\n<p class=\"para\">\n");
+for (i in json.References) {
+  document.write("<a href=\"#"+ (alpha[i]) +"\">("+ (alpha[i]) +") " + (json.References[i].attribution) + "</a><br>\n");
+  r.push("(" + (alpha[i]) + ")  " + (json.References[i].attribution) + " " + (json.References[i].source));
+}
+// introduction
+  document.write("<a id=\"introduction\"></a>\n<p class=\"head\">Introduction</p>\n");
+for (i in json.Introduction) {
+  document.write("<p class=\"para\">" + (json.Introduction[i]) + "</p>\n");
+}
+// congruent article
+for (i in json.Article) {
+  if (json.Article[i].heading) {
+    document.write("<p class=\"head\">" + (json.Article[i].heading) + "</p>\n");
+      for (k in json.Article[i].Para) {
+        document.write("<p class=\"para\">" + (json.Article[i].Para[k]) + "</p>\n");
+      }
+    }
+// jargon and notes
+if (json.Article[i].rule) {
+ document.write("<p class=\"rule\">" + (json.Article[i].rule) + "</p>\n");
+ document.write("<p class=\"jargon\"><span class=\"jargon-tag\">Jargon</span>" + (json.Article[i].jargon.replace(/\[/g, "[<jar>").replace(/\]/g, "</jar>]")));
+  if (json.Article[i].note) {
+   var x = y + 1;
+   document.write("<sup><a id=\"" + (x) + "\" href=\"#id" + (x) + "\">" + (x) + "</a></sup>");
+   var y = x;
+  }
+  document.write("</p>\n");
+ }
+}
+// disclaimer
+  document.write("</p>\n<a id=\"disclaimer\"></a>\n<p class=\"head\">Disclaimer</p>\n");
+for (i in Congruent.Disclaimer) {
+  document.write("<p class=\"para\">" + (Congruent.Disclaimer[i]) + "</p>\n");
+}
+// copyright
+  document.write("<a id=\"copyright\"></a>\n<p class=\"head\">Copyright</p>\n");
+for (i in Congruent.Copyright) {
+  document.write("<p class=\"para\">" + (Congruent.Copyright[i]) + "</p>\n");
+}
+// references
+ document.write("<div class=\"references\">\n<p class=\"title\"><a id=\"references\">References</a></p>\n");
+for (i in json.References) {
+ document.write("<a id=\"" + (alpha[i]) + "\"></a>\n<p id=\"col1\" class=\"note\">(" + (alpha[i]) + ") " + (json.References[i].attribution) + " " + (json.References[i].source) + "</p>\n");
+ }
+// notes 
+ document.write("</div><div class=\"references\">\n<p class=\"title\"><a id=\"notes\">Notes</a></p>\n");
+  for (i in json.Article) {
+   if (json.Article[i].note) {
+    var x = n + 1;
+    document.write("<p id=\"col1\" class=\"note\"><a id=\"id" + (x) + "\"></a><a href=\"#" + (x) + "\"><sup>" + (x) + "</sup></a>" + (json.Article[i].note) + "</p>");
+    var n = x;
+    }
+  }
+// congruence
+ document.write("</div><div class=\"references\">\n<p class=\"title\"><a id=\"congruence\">Congruence</a></p>\n<div class=\"card\">\n<p id=\"col8\" style=\"margin-top:-1rem;\">" + (json.Metadata.UID) + "</p>\n<p id=\"col8\" style=\"margin-top:-1rem;\">Congruent Draft<br>" + (json.Metadata.Draft.date) + "</p>\n<p id=\"col6\">" + (json.Metadata.Draft.Author.name) + "</p>\n<hr>\n<p>" + (json.Metadata.Draft.Author.industry) + "<br>" + (json.Metadata.Draft.Author.title) + "<br>" + (json.Metadata.Draft.Author.social) + "</p>\n\n</div>");
+ document.write("</div><div class=\"references\">\n<p class=\"title\"><a id=\"jargon\"><span id=\"jargon-logo\">Jargon Rules</span></a></p>\n");
+for (i in jargon) {
+  document.write("<p id=\"col0\" class=\"rule\">" + (jargon[i].rule) + "</p><p id=\"col1\" class=\"jargon\">" + (jargon[i].jargon.replace(/\[/g, "[<code>").replace(/\]/g, "</code>]")) + "</p>\n");
+}
+document.write("</div>\n</div>\n</div>\n</body>\n</html></textarea>");
+document.write("<p><span style=\"font-size:130%;color:tomato\">&cong;</span> Copy and paste the above text to a file named: <input type='text' size='40' name='Filename' value='" + (json.Metadata.topic) + ".html'></p>");
+if (json.Metadata.UID === null) {
+ document.write("<p><span style=\"font-size:130%;color:tomato\">&cong;</span> Copy and paste this <b>UID</b> into your <em><b>json</b></em> file under <kbd>Metadata:UID:</kbd> <input type='text' size='12' name='Date' value='" + (Date.now()) + "'></p>");
+}
+document.write("</div>");
---
+++ congruent.js
@@ -0,0 +1,116 @@
+/*                            C O N G R U E N T - 2 0 1 6
+ *                            ===========================
+ * Congruent is a collaborative review  process  that utilizes [Jargon: a technical
+ * framework] that enables authors to write more concise  sentences and phrases for
+ * their works. Congruent, as a process allow authors to ensure standards are clear
+ * to improve quality and performance while promoting original intent.
+ *
+ * There are several Congruent {objects} which are necessary to remain uniform and 
+ * Congruent. These Congruent headings are the Abstract, Disclaimer, and Copyright.
+ * These topic headings will be maintained by Congruent in the below Congruent.obj
+*/
+var Congruent = {
+  "Abstract": {
+    "Draft": [
+      "A draft article is the fist step in the Congruent process and is considered an opinion piece. It is possible for a draft article to stand on its own. However, it becomes stronger once it receives a peer review and collaborated into a Congruent status. A Congruent document is considered a very reliable source."
+    ],
+    "Review": [
+      "An article with a Review status is the second step in the Congruent process and is considered a collaborated effort between two professionals. An article in a Review status is able to stand on its own. However, it becomes stronger once it is reviewed at least by another third peer and collaborated into as Congruent. A article with a Review status is dependable, a Congruent document is considered a very reliable source."
+    ],
+    "Congruent": [
+      ""
+    ]
+  },
+  "Disclaimer": [
+    "The intent of this document is to share best practices and standards. The information contained within this document is published in good faith and for general information purposes only. There are no warranties about the completeness, reliability, and/or accuracy of this information. Please be advised that risk and discretion must be considered before utilizing information within this document."
+  ],
+  "Copyright": [
+    "Congruent is a professional interpretation of applicable federal laws, Executive Orders, directives, policies, regulations, standards, and guidance to include other authors works. Congruent authors reference and cite the work their articles are based, and credit any copyrighted works. The work of Congruent authors are their own."
+  ]
+}
+/* random Jargon rules {json object} with javascript
+ * The Congruent process relies solely on the Jargon Framework and therefore
+ * it is important to include the Jargon rule frames within this Congruent
+ * container for reference. This json object contains the nine Jargon Rule Frames 
+ * from: https://github.com/chaffin/Jargon  
+*/
+var jargon = [
+  {
+    "rule": "1. Rule frame [morpheme]",
+    "jargon": "There are [morpheme] that Jargon recognizes as beneficial to always frame. The author has the option to add more or less to ensure their work is comprehensible. This rule states that the morpheme's [and], [but], [if], [no], [not], [or], [will] are all identified by Jargon as key morphemes that should be framed, [or not]."
+  },
+  {
+    "rule": "2. Rule frame [morpheme:define]",
+    "jargon": "Sometimes a word means something to one person, and something different to another. This frame rule allows the author to add a [few:not greater than four] words to ensure the meaning. Yes, that is Jargon defining the maximum number of words to use when applying this rule."
+  },
+  {
+    "rule": "3. Rule frame [#morpheme]",
+    "jargon": "Jargon is not intended to [#create] fluffy bloated [#sentences]. This rule allows the [#author] to hash-tag words for use latter in the same [writing|#sentences] to make [their|#author] statements more clear and [show|#create] relationships."
+  },
+  {
+    "rule": "4. Rule frame [morpheme|#morpheme]",
+    "jargon": "Tagging a morpheme with a hash in rule three is much like [#setting] a variable. As the reader moves along a sentence they will find the hash tagged morpheme again, and realize that the author is [linking|#setting] a relationship."
+  },
+  {
+    "rule": "5. Rule frame [morpheme|related]",
+    "jargon": "This Jargon rule frame is [similar|related] to rule [two|2] with emphasis on related. There are [6|six] Jargon frames within this rule that fully [displays|shows] this rule frame in action. The reader will [notice|see] the relationships to fully [understand|realize] the authors intent."
+  },
+  {
+    "rule": "6. Rule frame [morpheme(synonym)]",
+    "jargon": "This rule frame [necessitates(requires)] the morpheme contains its synonym. Doing so allows the [author(writer)] to [hone(sharpen)] their words to ensure meaning is clearly understood. Do not [mistake(error)] this rule with rule frame [5|five], which they are not related."
+  },
+  {
+    "rule": "7. Rule frame [morpheme|related(synonym)]",
+    "jargon": "This rule frame [combines|adds(appends)] rule frames five and six. There is nothing better to [say|talk(verbalize)] about it."
+  },
+  {
+    "rule": "8. Rule frame [morpheme|~unrelated]",
+    "jargon": "This rule frame is the opposite of rule frame five. The [author|~reader] chooses the morpheme to negate, rather than frame. This rule allows the author to completely rule-out any stray thoughts the reader may form while understanding the work. Remember, Jargon [rules|~recommendations]."
+  },
+  {
+    "rule": "9. Rule frame [morpheme|*example]",
+    "jargon": "There are some morphemes that require examples to make a point. This frame rule relies on [short|*small,*quickly stated,*pointed] examples to frame a morpheme more clearly. Unlike rule frame two, this rule frame is not defined but rather an example, which is similar to the grammar rule that utilizes \"Latin e.g.\" when one is not intending to list everything."
+  }
+]
+function myFunction() {
+  var x = Math.floor((Math.random() * 8) + 1);
+  document.getElementById("quote").innerHTML = "<h4>" + (jargon[x].rule) + "</h4><p>" + (jargon[x].jargon.replace(/\[/g, "[<code>").replace(/\]/g, "</code>]")) + "</p>";
+}
+/*
+ * Slidemenu - Jason Howmans
+ * http://codepen.io/jasonhowmans/pen/dykhL/
+ * Congruent Slidemenu inspired by the works of Jason Howmans.
+ * HTML and CSS hacked to provide style and functionality for
+ * Congruent. Howmans javascript was wrapped using jQuery's 
+ * $(document).ready(funtion) to ensure it activated after all
+ * included files fully loaded.
+ * Thanks Jason:http://codepen.io/jasonhowmans/
+*/ 
+$(document).ready(function() {
+  var $body = document.body,
+   $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+    if (typeof $menu_trigger !== 'undefined') {
+     $menu_trigger.addEventListener('click', function() {
+     $body.className = ($body.className == 'menu-active') ? '' :'menu-active';
+     });
+   }
+});
+/*
+ * .slideToggle() - jQuery 
+ * Congruent utilizes jQuery .sligToggle() function to display and hide
+ * matched elements with a sliding motion. The element classes that are
+ * displayed or hidden match Congruent's (.rule) and (.jargon) classes 
+ * http://api.jquery.com/slideToggle/
+*/
+$(document).ready(function() {
+    $(".jargon").slideUp(0);
+    $(".rule").click(function() {
+        $(this).next(".jargon").slideToggle("slow");
+    });
+    $(".expand").click(function() {
+        $(".jargon").slideDown();
+    });
+    $(".closeall").click(function() {
+        $(".jargon").slideUp();
+    });
+});
